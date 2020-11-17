2c6000bd57631ff879bb097eafbd13d137de7973
Sergey Ponomarev <stokito@gmail.com>
2020-11-17 13:00:43 +0200

    ArrayList

---
+++ src/test/java/com/github/stokito/interview/AList.java
@@ -0,0 +1,35 @@
+package com.github.stokito.interview;
+
+public class AList<E>  {
+    public AList() {
+
+    }
+
+    public AList(int initialCapacity) {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return -1;
+    }
+
+    public void add(int index, E e) {
+
+    }
+
+    public E get(int index) {
+        return null;
+    }
+
+    public void set(int index, E e) {
+
+    }
+
+    public E remove(int index) {
+        return null;
+    }
+}
---
+++ src/test/java/com/github/stokito/interview/ArrayListTest.java
@@ -0,0 +1,82 @@
+package com.github.stokito.interview;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class ArrayListTest {
+
+    private AList<String> list = new AList<>();
+
+    @Test
+    public void testListInit() {
+        assertTrue(list.isEmpty());
+        assertEquals(0, list.size());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidCapacity() {
+        list = new AList<>(-1);
+    }
+
+    @Test
+    public void testAddElements() {
+        list.add(0, "Karol");
+        list.add(1, "Vanessa");
+        list.add(2, "Amanda");
+
+        assertEquals("Karol", list.get(0));
+        assertEquals("Vanessa", list.get(1));
+        assertEquals("Amanda", list.get(2));
+
+        list.add(1, "Mariana");
+
+        assertEquals("Karol", list.get(0));
+        assertEquals("Mariana", list.get(1));
+        assertEquals("Vanessa", list.get(2));
+        assertEquals("Amanda", list.get(3));
+
+        assertEquals(4, list.size());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testAddElementNull() {
+        list.add(0, null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testSetElementNull() {
+        list.add(0, "Kheyla");
+        list.set(0, null);
+    }
+
+    @Test
+    public void testSetElement() {
+        list.add(0, "Karol");
+        list.add(1, "Vanessa");
+        list.add(2, "Amanda");
+
+        list.set(1, "Livia");
+
+        assertEquals("Karol", list.get(0));
+        assertEquals("Livia", list.get(1));
+        assertEquals("Amanda", list.get(2));
+    }
+
+    @Test
+    public void testRemoveElement() {
+        list.add(0, "Karol");
+        list.add(1, "Vanessa");
+        list.add(2, "Amanda");
+
+        assertEquals("Amanda", list.remove(2));
+        assertEquals(2, list.size());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testRemoveWithEmptyList() {
+        list.remove(0);
+    }
+}
